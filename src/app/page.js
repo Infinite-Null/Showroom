e176315e3b9d10837ab03f026c1fb4ad11f1c3fe
Ankit Kumar Shah <ankit.shah@rtcamp.com>
2025-04-24 17:23:16 +0530

    Fix import paths in page.js for consistency

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 
-import Aboutus from "../components/global/home/AboutUs";
-import Carousel from "../components/global/home/Carousel";
-import GetInTouch from "../components/global/home/GetInTouch";
-import OurShowrooms from "../components/global/home/OurShowrooms";
-import OurVehicles from "../components/global/home/OurVehicles";
+import Aboutus from "@/components/global/Home/AboutUs";
+import Carousel from "@/components/global/Home/Carousel";
+import GetInTouch from "@/components/global/Home/GetInTouch";
+import OurShowrooms from "@/components/global/Home/OurShowrooms";
+import OurVehicles from "@/components/global/Home/OurVehicles";
 import NavBar from "../components/global/NavBar";
 import './animation.css';
 
