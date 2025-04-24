3ebf33321499cf1c33cd48c61c3b12f91ba91aa0
Ankit Kumar Shah <ankit.shah@rtcamp.com>
2025-04-24 17:12:02 +0530

    Fix import paths in Home component for consistency

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
 
-import Aboutus from "@/components/global/home/AboutUs";
-import Carousel from "@/components/global/home/Carousel";
-import GetInTouch from "@/components/global/home/GetInTouch";
-import OurShowrooms from "@/components/global/home/OurShowrooms";
-import OurVehicles from "@/components/global/home/OurVehicles";
-import NavBar from "@/components/global/NavBar";
+import Aboutus from "../components/global/home/AboutUs";
+import Carousel from "../components/global/home/Carousel";
+import GetInTouch from "../components/global/home/GetInTouch";
+import OurShowrooms from "../components/global/home/OurShowrooms";
+import OurVehicles from "../components/global/home/OurVehicles";
+import NavBar from "../components/global/NavBar";
 import './animation.css';
 
 export default function Home() {
