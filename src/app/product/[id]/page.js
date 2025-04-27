bef293a1134d47fd25744d582b1a95a4fea9ed16
Ankit Kumar Shah <ankit.shah@rtcamp.com>
2025-04-27 17:27:38 +0530

    Add ProductDetailPage and ProductFilterPage components with NavBar integration

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -1,9 +1,10 @@
 import NavBar from '@/components/global/Navbar/NavBar'
 
-export default function ProductPage() {
+export default function ProductDetailPage() {
   return (
     <>
         <NavBar/>
+        <div>hdsafjkhdfsj</div>
     </>
   )
 }
---
+++ src/app/product/type/[type]/page.js
@@ -0,0 +1,10 @@
+import NavBar from '@/components/global/Navbar/NavBar'
+
+export default function ProductFilterPage() {
+  return (
+    <>
+        <NavBar/>
+        <div>Type</div>
+    </>
+  )
+}
